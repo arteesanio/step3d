32a073e0825b0a6a0365619895c8828fe9715a37
Abraham Duno <tresduno@gmail.com>
2024-11-09 21:08:59 -0400

    telegram script import unimport 333

--- src/app/win/page.tsx
+++ src/app/win/page.tsx
@@ -5,10 +5,6 @@ import { GameContainer } from '@/GameContainer'
 export default function WinPage() {
   return (
     <main className={styles.main}>
-      <Script
-          src="https://telegram.org/js/telegram-web-app.js"
-          strategy="beforeInteractive" // ensures the script loads before the page renders
-        />
       <GameContainer initialLevel="win" />
     </main>
   )
